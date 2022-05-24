87666cc71cb474443914a5499b7adf3efccbfa2f
Ieva Bidermane <ibidermane@gmail.com>
2022-05-24 17:43:16 +0200

    added MLflow for keeping track of model params

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,4 +1,6 @@
 # imports
+import joblib
+from termcolor import colored
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from TaxiFareModel.encoders import DistanceTransformer,TimeFeaturesEncoder
@@ -7,6 +9,12 @@ from TaxiFareModel.data import clean_data, get_data
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import mlflow
+from  mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
+
+MLFLOW_URI = "https://mlflow.lewagon.ai/"
+EXPERIMENT_NAME = "[DE] [Berlin] [ievabi] TaxiFareModel v1"
 
 
 class Trainer():
@@ -18,6 +26,12 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        # for MLFlow
+        self.experiment_name = EXPERIMENT_NAME
+
+    def set_experiment_name(self, experiment_name):
+        '''defines the experiment name for MLFlow'''
+        self.experiment_name = experiment_name
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -47,6 +61,7 @@ class Trainer():
     def run(self):
         """set and train the pipeline"""
         self.set_pipeline()
+        self.mlflow_log_param("model", "Linear")
         self.pipeline.fit(self.X,self.y)
 
 
@@ -54,7 +69,38 @@ class Trainer():
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
-        return rmse
+        self.mlflow_log_metric("rmse", rmse)
+        return round(rmse, 2)
+
+    def save_model(self):
+        """Save the model into a .joblib format"""
+        joblib.dump(self.pipeline, 'model.joblib')
+        print(colored("model.joblib saved locally", "green"))
+
+# MSFlow params for tracking
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
 
 if __name__ == "__main__":
 # get data
@@ -68,7 +114,9 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 # train
     model = Trainer(X_train,y_train) #initiate trainer
+    model.set_experiment_name('[DE] [Berlin] [ievabi] TaxiFareModel v1')
     model.run()
 
 # evaluate
-    print(model.evaluate(X_test, y_test))
+    rmse= model.evaluate(X_test, y_test)
+    print(f"rmse: {rmse}")
